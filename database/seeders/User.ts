d73ebe631bb103423291f500cfc13005e53d64a6
filip207 <polic.filip99@gmail.com>
2021-06-07 00:39:02 -0500

    chore: no seed

--- database/seeders/User.ts
+++ database/seeders/User.ts
@@ -3,27 +3,27 @@ import User from 'App/Models/User'
 
 export default class UserSeeder extends BaseSeeder {
   public async run () {
-    await User.createMany([
-      {
-        email: 'a@test.com',
-        username: 'a',
-        firstName: 'a',
-        lastName: 'a',
-        gender: 'male',
-        contactNumber: '1234567890',
-        password: '123',
-        address: 'a',
-      },
-      {
-        email: 'b@test.com',
-        username: 'b',
-        firstName: 'b',
-        lastName: 'b',
-        gender: 'female',
-        contactNumber: '1234567890',
-        password: '123',
-        address: 'b',
-      }
-    ])
+    // await User.createMany([
+    //   {
+    //     email: 'a@test.com',
+    //     username: 'a',
+    //     firstName: 'a',
+    //     lastName: 'a',
+    //     gender: 'male',
+    //     contactNumber: '1234567890',
+    //     password: '123',
+    //     address: 'a',
+    //   },
+    //   {
+    //     email: 'b@test.com',
+    //     username: 'b',
+    //     firstName: 'b',
+    //     lastName: 'b',
+    //     gender: 'female',
+    //     contactNumber: '1234567890',
+    //     password: '123',
+    //     address: 'b',
+    //   }
+    // ])
   }
 }
